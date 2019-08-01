8cd4a86d878544a5bd715e4bbe58daa6b36813e0
success <nkonwanasuccess@gmail.com>
2019-08-01 02:41:15 -0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController, IonGrid } from '@ionic/angular';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+coffee:string='';
+prices:number=0;
+added:string='';
+addedP:number=0;
+  constructor(public alertController: AlertController) {}
+  yourPrices(price: number){
+    this.prices=this.prices+price;
+  }
+  yourCoffee(cup:string){
+    this.coffee=this.coffee+cup+', ';
+  }
+// added item to ur coffee
+  addedItem(add:string){
+    this.added=this.added+add+', ';
+  }
+  openForm() {
+    document.getElementById("myForm").style.display = "block";
+    this.x=document.getElementById("tempGrid");
+     this.x.style.display = "block";
+         
+  }
+  
+  closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  cancel() {
+        this.coffee='';
+        this.prices=0;
+        this.added='';
+      }
+      printContents:any;
+      originalContents:any;
+      x:any;
+  print(){
+          this.x=document.getElementById("tempGrid");
+          if (this.x.style.display === "none") {
+           this.x.style.display = "block";
+          } else {
+            this.x.style.display = "none";
+          }
+          window.print();
+          this.coffee='';
+          this.prices=0;
+          this.added='';
+   }
+   grid:any;
+  removeGrid(gridState){
+this.grid=document.getElementById("tempGrid");
+// this.grid.
+  }
 }
